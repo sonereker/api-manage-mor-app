60e94aa0a7b8a5263f64e5950c97a087fa117ccd
Soner Eker <sonereker@gmail.com>
2019-01-15 23:31:21 +0300

    Introduce CORS handler add `log.Fatal` for server

--- main.go
+++ main.go
@@ -4,17 +4,23 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	. "github.com/sonereker/api-manage-mor-app/handler"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	r := mux.NewRouter()
+
 	r.Handle("/authenticate", AuthenticationHandler).Methods("POST")
 	r.Handle("/collection", ValidateToken(ViewCollectionHandler)).Methods("GET")
 	r.Handle("/collection", ValidateToken(AddWorkHandler)).Methods("POST")
 	r.Handle("/collection/work/{id}", ValidateToken(UpdateWorkHandler)).Methods("PUT")
 	r.Handle("/collection/work/{id}", ValidateToken(DeleteWorkHandler)).Methods("DELETE")
 
-	_ = http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
+	headers := handlers.AllowedHeaders([]string{"X-Requested-With", "Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"})
+	origins := handlers.AllowedOrigins([]string{"*"})
+	methods := handlers.AllowedMethods([]string{"GET", "HEAD", "POST", "PUT", "OPTIONS", "DELETE"})
+
+	log.Fatal(http.ListenAndServe(":4000", handlers.LoggingHandler(os.Stdout, handlers.CORS(headers, origins, methods)(r))))
 }
